291dad2f4a652158f71885d7af8632c2b3380cae
Omer Shacham <Omertorrents@gmail.com>
2018-10-28 11:40:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import time
 
 for i in range(100):
   print("test-%s"%i)
-  with open("output/file-%s.txt"%random.randint(0,100), "w") as f:
+  with open("output/file-%s.txt"%random.randint(0,100), "w+") as f:
     f.write("hello123%s"%random.randint(0,1000))
     f.write("hello123%s"%random.randint(0,1000))
     f.write("hello123%s"%random.randint(0,1000))
