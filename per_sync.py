7107549da4336b5612d7cb5addf8a037102a08a8
Omer Shacham <Omertorrents@gmail.com>
2019-01-14 18:46:43 +0200

    Update per_sync.py

--- per_sync.py
+++ per_sync.py
@@ -1,9 +1,13 @@
 import random
 import time
-
+import os
+if(!os.path.isdir('output')):
+  os.mkdir('output')
+  
+ 
 for i in range(1000):
   n = random.randint(0,10000)
-  f = open("new-file-%s"%n, "w+")
+  f = open("output/new-file-%s"%n, "w+")
   f.write("Wowwwwww")
   f.write("%s"%n)
   print("File Created!!!! n=%s"%n) 
