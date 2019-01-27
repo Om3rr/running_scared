29390bc4b81533e5a5da72222a3a34761a4e9b52
Omer Shacham <Omertorrents@gmail.com>
2019-01-27 17:36:49 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -3,6 +3,6 @@ import random
 
 
 def is_it_dog(dog):
-  app = current_app()
+  app = current_app._get_current_object()
   sol = "not " if random.choice([True, False]) else ""
   return app.config['hazil']
