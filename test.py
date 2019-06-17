82b485cdc61a0bff0ba63024553f683a67f8fc93
omeriko <omertorrents@gmail.com>
2019-06-17 08:41:41 +0000

    Experiment 9 debugging session fixes

--- test.py
+++ test.py
@@ -3,10 +3,4 @@ import time
 import os
 for i in range(5):
   print("test-%s"%i)
-  with open("output/file-%s.txt"%random.randint(0,100), "w+") as f:
-    f.write("hello123%s"%random.randint(0,1000))
-    f.write("hello123%s"%random.randint(0,1000))
-    f.write("hello123%s"%random.randint(0,1000))
-    f.write("hello123%s"%random.randint(0,1000))
-    f.write("hello123%s"%random.randint(0,1000))
   time.sleep(5)
